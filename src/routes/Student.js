3d8a501f33c85f1be969c45a9b6ec5f5a34ca5c4
unknown <josedanielramosbarquero@gmail.com>
2021-12-03 11:58:10 -0600

    agrego correcion de Students

--- src/routes/Student.js
+++ src/routes/Student.js
@@ -1,5 +1,7 @@
-import { useState, useEffect } from "react"
-import { useParams } from "react-router-dom"
+import { useState } from "react"
+import { useParams } from "react-router"
+import { Link } from "react-router-dom"
+import { useEffect } from "react/cjs/react.development"
 
 const Student = () => {
     const [student, setStudent] = useState({})
@@ -11,7 +13,7 @@ const Student = () => {
         method: 'GET',
         headers: {
             'x-hasura-admin-secret': process.env.REACT_APP_HASURA_SECRET
-        },
+        }
         }).then(response => response.json())
         .then(result => {
             setIsLoaded(true)
@@ -28,6 +30,7 @@ const Student = () => {
     return (
         <>
             <h1>Student Profile</h1>
+            <Link to={`updateStudent/${params.StudentId}`}>Update Student</Link>
             <hr />
             <h2>Name:</h2>
             <span>{student?.name}</span>
